74856b27341fc97ebcd1a740e52cec374f602d6f
nothingx-x <sirmrmmr@gmail.com>
2025-11-23 16:25:02 +0330

    Fix Cube volume calculation

--- src/main/kotlin/xyz/nothing/solidexample/models/shapes/solid/Cube.kt
+++ src/main/kotlin/xyz/nothing/solidexample/models/shapes/solid/Cube.kt
@@ -5,7 +5,7 @@ import xyz.nothing.solidexample.models.I3DShape
 import kotlin.math.pow
 
 class Cube (val side: Double): I3DShape{
-    override fun volume(): Double = side * 3
+    override fun volume(): Double = side.pow(3)
 
     override fun surfaceArea(): Double = side.pow(2) * 6
 }
